60a00396fd92927c10d63ddca2a151e4f9edbea4
unknown <narminna@code.edu.az>
2023-06-24 15:04:57 +0400

    Inheritance

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Inheritance
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+
+        public Person(string name, string surname, int age)
+        {
+            Name = name;
+            Surname = surname;
+            Age = age;
+        }
+    }
+    public class Teacher : Person
+    {
+        public void Explain()
+        {
+            Console.WriteLine("Explaining...");
+        }
+    }
+    public class Student : Person
+    {
+        public void Learn()
+        {
+            Console.WriteLine("Learning...");
+        }
+    }
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Teacher teacher = new Teacher("Araz", "Yusubov", 48);
+            Student student = new Student("Ali", "Aliyev", 18);
+
+            Console.WriteLine(teacher.Name + " " + teacher.Surname + " " + teacher.Age);
+            Console.WriteLine(student.Name + " " + student.Surname + " " + student.Age);
+            teacher.Explain();
+            student.Learn();
+        }
+    }
+}
